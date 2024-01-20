87c0996d31f1bcf444de95c1965ff5cd431dc887
PriyanshBastawale11 <122052017+PriyanshBastawale11@users.noreply.github.com>
2024-01-20 07:30:16 +0000

    added new code

--- app.py
+++ app.py
@@ -5,3 +5,22 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
     return render_template("index.html", title="Hello")
+
+@app.route("/Hello")
+def Hello():
+    return "hello"
+
+@app.route("/name")
+def name():
+    return "Priyansh"
+
+@app.route("/age")
+def age():
+    return "19"
+
+@app.route("/city")
+def city():
+    return "nagpur"
+
+
+
